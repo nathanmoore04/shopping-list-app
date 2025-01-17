86d46925796cb3a3cc63c71d04bf9f44b1fad4f6
SCM-Grievous <57879138+SCM-Grievous@users.noreply.github.com>
2025-01-17 12:47:15 -0500

    Set up recipes endpoint

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
-const { createServer } = require("node:http")
+const express = require('express');
+const app = express();
 
-const hostname = "127.0.0.1";
-const port = 3000
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain')
-    res.end("Hello World")
-})
+app.use(express.json());
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+// Test route, just lets you know the server is running
+app.get('/', (req, res) => {
+    res.send("Service is up and running");
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
+
+// Example recipes JSON objects
+const recipes = [
+    { id: 1, name: 'Spaghetti', ingredients: ['Pasta', 'Tomato Sauce'], steps: ['Do something', 'do something else']},
+    { id: 2, name: 'Pancakes', ingredients: ['Flour', 'Milk', 'Eggs'], steps: ['Do something', 'do something else']}
+];
+
+// Recipes endpoint
+app.get('/recipes', (req, res) => {
+    res.json(recipes);
+});
