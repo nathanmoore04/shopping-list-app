60b48289e532f9f90c4af3d0ad84dcb17d6ad0e4
SCM-Grievous <57879138+SCM-Grievous@users.noreply.github.com>
2025-01-18 14:26:13 -0500

    Add database connection, update GET and POST

--- server/server.js
+++ server/server.js
@@ -1,9 +1,31 @@
+// Imports
 const express = require('express');
+const { Pool } = require('pg');
+
+// ----- Server stuff -----
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const SERVER_PORT = 3000;
 
+// ----- DB stuff -----
+const pool = new Pool({
+    user: 'postgres', // Default superuser
+    host: 'localhost', // Server address
+    database: 'recipes_app_test', // Database name
+    password: 'N8K9$wims', // Replace with your password
+    port: 5432, // Default PostgreSQL port
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+    } else {
+        console.log('Database connected');
+    }
+});
+
+// ----- App stuff ------
 app.use(express.json());
 
 // Test route, just lets you know the server is running
@@ -11,34 +33,35 @@ app.get('/', (req, res) => {
     res.send("Service is up and running");
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(SERVER_PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${SERVER_PORT}`);
 });
-
-// Example recipes JSON objects
-const recipes = [
-    { id: 1, name: 'Spaghetti', ingredients: ['Pasta', 'Tomato Sauce'], steps: ['Do something', 'do something else']},
-    { id: 2, name: 'Pancakes', ingredients: ['Flour', 'Milk', 'Eggs'], steps: ['Do something', 'do something else']}
-];
 
 // ----- Recipes endpoint -----
 // GET route - list all recipes
-app.get('/recipes', (req, res) => {
-    res.json(recipes);
+app.get('/recipes', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM recipes');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 // POST route - add a new recipe
-app.post('/recipes', (req, res) => {
+app.post('/recipes', async (req, res) => {
     const { name, ingredients, steps } = req.body;
-    const newRecipe = {
-        id: recipes.length + 1,
-        name,
-        ingredients,
-        steps
-    };
-
-    recipes.push(newRecipe);
-    res.status(201).json(newRecipe);
+    try {
+        const result = await pool.query(
+            'INSERT INTO recipes (name, ingredients, steps) VALUES ($1, $2, $3) RETURNING *',
+            [name, ingredients, steps]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 // PUT route - update existing recipe
