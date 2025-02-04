045f0b064e533f5b3b91e55242591e291d6f6b21
SCM-Grievous <57879138+SCM-Grievous@users.noreply.github.com>
2025-02-04 13:44:42 -0500

    Begin developing dashboard

--- client/src/vue/DashboardPage.vue
+++ client/src/vue/DashboardPage.vue
@@ -3,10 +3,76 @@ import Navbar from './Navbar.vue';
 </script>
 
 <template>
-<Navbar />
-<h1>Dashboard</h1>
+    <Navbar />
+    <div class="container">
+        <h1 class="fw-bold">Dashboard</h1>
+        <h3 class="mt-2 fw-bold">Your recent meals</h3>
+
+        <div class="row align-items-start">
+            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
+                <div class="card bg-tertiary" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Meal title</h5>
+                        <ul class="card-text">
+                            <li>Ingredient 1</li>
+                            <li>Ingredient 2</li>
+                            <li>Ingredient 3</li>
+                            <li>and [num] more...</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
+                <div class="card bg-tertiary" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Meal title</h5>
+                        <ul class="card-text">
+                            <li>Ingredient 1</li>
+                            <li>Ingredient 2</li>
+                            <li>Ingredient 3</li>
+                            <li>and [num] more...</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
+                <div class="card bg-tertiary" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Meal title</h5>
+                        <ul class="card-text">
+                            <li>Ingredient 1</li>
+                            <li>Ingredient 2</li>
+                            <li>Ingredient 3</li>
+                            <li>and [num] more...</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
+                <div class="card bg-tertiary" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Meal title</h5>
+                        <ul class="card-text">
+                            <li>Ingredient 1</li>
+                            <li>Ingredient 2</li>
+                            <li>Ingredient 3</li>
+                            <li>and [num] more...</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h3 class="mt-2 fw-bold">Your recent plans</h3>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
