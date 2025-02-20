4bbfc1b0071e6e5b54bc94e21fd2664652b60186
SCM-Grievous <57879138+SCM-Grievous@users.noreply.github.com>
2025-02-20 13:22:43 -0500

    Create dynamic meal cards on dashboard

--- client/src/vue/DashboardPage.vue
+++ client/src/vue/DashboardPage.vue
@@ -1,13 +1,37 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import axios from 'axios';
 import Navbar from './Navbar.vue';
 import Footer from './Footer.vue';
-import { useRouter } from 'vue-router';
+import MealCard from './MealCard.vue';
 
-const router = useRouter();
+const authStore = useAuthStore();
+
+const errorMessage = ref('');
+const meals = ref([]);
+
+onMounted(async () => {
+    const token = authStore.token;
+    errorMessage.value = '';
+
+    try {
+        // Get all meals for user
+        const response = await axios.get('http://127.0.0.1:3000/recipes/', {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+
+        meals.value = response.data;
+    } catch (err) {
+        errorMessage.value = 'Error fetching user data. Please try again.';
+        console.error(err);
+    }
+})
 </script>
 
 <template>
     <Navbar />
+    <p v-if="errorMessage" class="text-danger">{{ errorMessage }}</p>
     <div class="container text-center py-5">
         <h1 class="fw-bold">Hello, [user name]</h1>
         <h3 class="fw-light">Message of the day</h3>
@@ -36,70 +60,12 @@ const router = useRouter();
             <div class="col">
                 <a href="#" class="btn btn-outline-primary float-end">See all meals <i class="bi bi-arrow-right"></i></a>
             </div>
-        </div>
+        </div>  
 
-        <div class="row align-items-start mt-2">
-            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="../assets/placeholder.svg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title fw-semibold">Meal title</h5>
-                        <ul class="card-text">
-                            <li>Ingredient 1</li>
-                            <li>Ingredient 2</li>
-                            <li>Ingredient 3</li>
-                            <li>and [num] more...</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="../assets/placeholder.svg" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title fw-semibold">Meal title</h5>
-                        <ul class="card-text">
-                            <li>Ingredient 1</li>
-                            <li>Ingredient 2</li>
-                            <li>Ingredient 3</li>
-                            <li>and [num] more...</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="../assets/placeholder.svg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title fw-semibold">Meal title</h5>
-                        <ul class="card-text">
-                            <li>Ingredient 1</li>
-                            <li>Ingredient 2</li>
-                            <li>Ingredient 3</li>
-                            <li>and [num] more...</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="../assets/placeholder.svg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title fw-semibold">Meal title</h5>
-                        <ul class="card-text">
-                            <li>Ingredient 1</li>
-                            <li>Ingredient 2</li>
-                            <li>Ingredient 3</li>
-                            <li>and [num] more...</li>
-                        </ul>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-            </div>
+        <div v-if="meals.length > 0" class="row align-items-start mt-2">
+            <MealCard v-for="meal in meals" :key="meal.id" :meal="meal" />
         </div>
+        <p v-else>No meals to display. Why not create one?</p>
 
         <div class="row mt-2">
             <div class="col">
---
+++ client/src/vue/MealCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const props = defineProps({
+    meal: Object,
+});
+
+const viewMeal = () => {
+    router.push(`/meals/${props.meal.id}`);
+}
+
+</script>
+
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center">
+        <div class="card">
+            <img :src="meal.image" class="card-img-top" alt="..." v-if="meal.image">
+            <div class="card-body">
+                <h5 class="card-title fw-semibold">{{ meal.name }}</h5>
+                <p v-if="meal.tags.length">
+                    <span v-for="(tag, index) in meal.tags" :key="index"
+                        class="badge bg-secondary me-2 d-inline-flex align-items-center px-2 py-1">
+                        {{ tag }}
+                    </span>
+                </p>
+                <p class="card-text">
+                    {{ meal.ingredients.length }} ingredients
+                </p>
+                <button class="btn btn-primary" @click="viewMeal">View details</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    transition: all 0.3s ease-in-out;
+    min-width: 18rem;
+}
+
+.card:hover {
+    transform: scale(1.02);
+}
+</style>
